adffe14f0a2c642d97bb9c5417c886497068fc15
Aissam Hammoutane <ahammout@e3r9p2.1337.ma>
2023-10-17 19:18:11 +0100

    update

--- inc/main.hpp
+++ inc/main.hpp
@@ -6,7 +6,7 @@
 /*   By: ahammout <ahammout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 15:07:18 by mwilsch           #+#    #+#             */
-/*   Updated: 2023/10/16 23:48:04 by ahammout         ###   ########.fr       */
+/*   Updated: 2023/10/17 18:37:18 by ahammout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,8 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ NUMERIC REPLIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 
-// This numeric reply needs to be send when the client connects to the server first time.
-// :server 001 <nick> :Welcome to the <network> Network, <nick>[!<user>@<host>]
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ REPLIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 #define RPL_WELCOME(nickname) "001 "  + nickname + " :Welcome to the IRC Network, " + nickname + "\r\n"
 #define RPL_NAMREPLY(nickname, channelType, channel, users) "353 " + nickname + " " + channelType + " " + channel + " :" + users + "\r\n"
@@ -46,19 +42,20 @@ using std::endl;
 #define RPL_INVITING(nickSender, channel, nickReciever) "341 " + nickSender + " " + nickReciever + " " + channel + "\r\n"
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ERRORS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+#define ERR_INVITEONLYCHAN(channel) "473: " + channel + " :Cannot join channel (+i)\r\n"
+#define ERR_BADCHANNELKEY(channel) "475: " + channel + " : Cannot join channel (+k)\r\n"
+#define ERR_CHANNELISFULL(channel) "471: " + channel + " :Cannot join channel (+l)\r\n"
+
 #define ERR_UNKNOWNCOMMAND(nickname, wrongCommand) "421 " + nickname + " " + wrongCommand + " :Unknown command\r\n"
 #define ERR_NONICKNAMEGIVEN() "431: No nickname given\r\n"
 #define ERR_NICKNAMEINUSE(nick) "433: " + nick + " :Nickname is already in use\r\n"
 #define ERR_NEEDMOREPARAMS(command) "461: " + command + " : Not enough parameters\r\n"
 #define ERR_ALREADYREGISTRED() "462: You may not reregister\r\n"
-#define ERR_BADCHANNELKEY(channel) "475: " + channel + " : Cannot join channel (+k)\r\n"
 #define ERR_NOTONCHANNEL(channel) "442: " + channel + " : You're not on that channel\r\n"
 #define ERR_USERONCHANNEL(user, channel) "443: " + user + " " + channel + " : is already on channel\r\n"
 #define ERR_CHANOPRIVSNEEDED(channel) "482: " + channel + " : You're not channel operator\r\n"
 #define ERR_NOSUCHCHANNEL(channel) "403: " + channel + " : No such channel\r\n"
-#define ERR_CHANNELISFULL(channel) "471: " + channel + " :Cannot join channel (+l)\r\n"
 #define ERR_UNKNOWNMODE(character) "472: " + character + " : is unknown mode char to me\r\n"
-#define ERR_INVITEONLYCHAN(channel) "473: " + channel + " :Cannot join channel (+i)\r\n"
 #define ERR_NOTREGISTERED(nickname, command) "451 " + nickname + " " + command + " :You must finish connecting with another nickname first.\r\n"
 #define ERR_PASSWDMISMATCH() "464 * :Password incorect\r\n"
 #define ERR_NORECIPIENT(target) "411 " + target + " :No recipient given (PRIVMSG)\r\n"
@@ -68,10 +65,4 @@ using std::endl;
 
 #define ERR_USERNOTINCHANNEL(nickSender, nickTarget, channel) "441 " +  nickSender + " " + nickTarget + " " + channel + " : They aren't on that channel\r\n"
 
-
-
-
-
 // PRIVMSG Numeric replies to add here.
-
-// #define ERR_NEEDMOREPARAMS() 
