b8e72367047e37086081e4c7d412821af2d5611f
Aissam Hammoutane <ahammout@e3r9p15.1337.ma>
2023-10-23 12:21:23 +0100

    Return back invite file was missing...

---
+++ src/channel/invite.cpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   invite.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahammout <ahammout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 12:20:25 by ahammout          #+#    #+#             */
+/*   Updated: 2023/10/23 12:20:27 by ahammout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ExecuteCommands.hpp"
+
+// void ExecuteCommands::invite(ServerReactor &_server, Message &Message, int clientSocket) {
+//     if (Message.getParams().size() < 2)
+//         throw ServerException(ERR_NEEDMOREPARAMS(Message.getCommand()));
+
+//     vector<string> params = Message.getParams();
+//     string channelName = params[1];
+//     ChannelData& channel = _server.getChannelManager().getChannelByName(channelName);
+
+//     if (!_server.doesChannelExist(params[1]))
+//         throw ServerException(ERR_NOSUCHCHANNEL(channelName));
+
+//     if (!channel.isCLient(clientSocket))
+//         throw ServerException(ERR_NOTONCHANNEL(channelName));
+
+//     if (!channel.isOperator(clientSocket))
+//         throw ServerException(ERR_CHANOPRIVSNEEDED(channelName));
+
+// 		if (_server.getClientManager().MatchNickName(channel.getClientSockets(), params[0]) != -1)
+// 				throw ServerException(ERR_USERONCHANNEL(params[0], channelName));
+
+//     int targetFD = _server.getClientManager().getClientSocketByNick(params[0]);
+//     if (targetFD == -1)
+//         throw ServerException(ERR_NOSUCHNICKCHANNEL(params[0], channelName));
+
+//     channel.addGuest(params[0]);
+//     _server.sendMsg_FixLater(targetFD, _server.createInfoMsg(_server.getClientDataFast(clientSocket), Message.getCommand(), params));
+// 		_server.sendNumericReply_FixLater(clientSocket, RPL_INVITING(_server.getClientDataFast(clientSocket).getNickname(), channelName, params[0]));
+// }
+
+
+void     ExecuteCommands::invite(ServerReactor &_server, Message &Message, int clientSocket){
+	ClientData  &client = _server.getClientDataFast(clientSocket);
+    if (Message.getParams().size() < 2) {
+        _server.sendNumericReply_FixLater(clientSocket, ERR_NEEDMOREPARAMS(client.getNickname(), Message.getCommand()));
+        return ;
+    }
+	vector<string>	params = Message.getParams();
+    string			channelName = params[1];
+	ChannelData&	channel = _server.getChannelManager().getChannelByName(channelName);
+	const string &nick = client.getNickname();
+	if (!_server.doesChannelExist(params[1])){
+		_server.sendNumericReply_FixLater(clientSocket, ERR_NOSUCHCHANNEL(nick, channelName));
+		return ;
+	}
+	if (!channel.isCLient(clientSocket)) {
+		_server.sendNumericReply_FixLater(clientSocket, ERR_NOTONCHANNEL(nick, channelName));
+		return ;
+	}
+    if (!channel.isOperator(clientSocket)) {
+        _server.sendNumericReply_FixLater(clientSocket, ERR_CHANOPRIVSNEEDED(channelName));
+        return ;
+    }
+	if (_server.getClientManager().MatchNickName(channel.getClientSockets(), params[0]) != -1) { 
+			_server.sendNumericReply_FixLater(clientSocket, ERR_USERONCHANNEL(params[0], channelName));
+	return ;
+	}
+	int targetFD = _server.getClientManager().getClientSocketByNick(params[0]);
+	if (targetFD == -1) {
+			_server.sendNumericReply_FixLater(clientSocket, ERR_NOSUCHNICKCHANNEL(params[0], channelName));
+			return ;
+	}
+	channel.addGuest(params[0]);
+	_server.sendMsg_FixLater(targetFD, _server.createMsg(client, Message.getCommand(), params));
+	_server.sendNumericReply_FixLater(clientSocket, RPL_INVITING(client.getNickname(), channelName, params[0]));
+}
